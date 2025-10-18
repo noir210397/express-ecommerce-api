23689283f9f042fe55bf5c6400d959756f6df4e5
Ubiagba Timothy <noir210397@gmail.com>
2025-10-18 11:10:39 +0100

    initial-commit

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["/src/server.ts"],
+    outDir: "dist",
+    target: "node20",
+    format: ["esm"],
+    clean: true,
+    sourcemap: true,
+    dts: false,
+    splitting: false,
+    minify: false,
+});
